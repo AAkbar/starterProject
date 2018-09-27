765182e1ce020c61415040bf5c60dcd491011d2f
Ali Akbar <ali.akbar102030@gmail.com>
2018-09-27 10:39:29 +0500

    gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+
+const gulp = require("gulp"),
+      concat = require("gulp-concat"),
+      uglify = require("gulp-uglify"),
+      sass = require("gulp-sass"),
+      rename = require("gulp-rename"),
+      prefixer = require("gulp-autoprefixer"),
+      maps = require("gulp-sourcemaps");
+
+gulp.task("default", () =>{
+    console.log("\n\t\tI am running");
+});
