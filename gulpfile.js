39d8d5b32e4662c0ab522b542d2991b003a6b11a
Ali Akbar <ali.akbar102030@gmail.com>
2018-09-27 12:44:28 +0500

    bunch of files for gulp added

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,46 @@ const gulp = require("gulp"),
       sass = require("gulp-sass"),
       rename = require("gulp-rename"),
       prefixer = require("gulp-autoprefixer"),
-      maps = require("gulp-sourcemaps");
+      maps = require("gulp-sourcemaps"),
+      imagemin = require("gulp-imagemin");
 
-gulp.task("default", () =>{
-    console.log("\n\t\tI am running");
+//gulp concat and uglify
+gulp.task("concatScript",()=>{
+    gulp
+        .src(["./js/jQuery.js",
+              "./js/main.js"])
+        .pipe(maps.init())
+        .pipe(concat("app.js"))
+        .pipe(maps.write("."))
+        .pipe(gulp.dest("./js"));
 });
+
+gulp.task("uglifyScript",()=>{
+    gulp
+        .src("./js/app.js")
+        .pipe(uglify())
+        .pipe(rename("app.min.js"))
+        .pipe(gulp.dest("./js"));
+});
+
+// gulp sass 
+gulp.task("sass",()=>{
+    gulp
+        .src("./scss/app.scss")
+        .pipe(maps.init())
+        .pipe(sass().on("error", sass.logError))
+        .pipe(prefixer({
+            browsers: ['last 6 versions'],
+            cascade: false}))
+        .pipe(maps.write("."))
+        .pipe(gulp.dest("./css"));
+});
+
+// gulp watch
+gulp.task("watch", ()=>{
+    gulp.watch("./js/*.js", ["concatScript", "uglifyScript"]);
+    gulp.watch("./**/*.scss", ["sass"]);
+});
+
+// default task
+gulp.task("default", ["concatScript", "uglifyScript", "sass", "watch"]);
