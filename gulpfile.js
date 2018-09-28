38054a0ea894d6820b793055ef00e403b20ceb21
Ali Akbar <ali.akbar102030@gmail.com>
2018-09-28 12:44:22 +0500

    My gulp all set for project

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-
+"use strict";
 const gulp = require("gulp"),
       concat = require("gulp-concat"),
       uglify = require("gulp-uglify"),
@@ -6,21 +6,22 @@ const gulp = require("gulp"),
       rename = require("gulp-rename"),
       prefixer = require("gulp-autoprefixer"),
       maps = require("gulp-sourcemaps"),
-      imagemin = require("gulp-imagemin");
+      imagemin = require("gulp-imagemin"),
+      del = require("del");
 
 //gulp concat and uglify
 gulp.task("concatScript",()=>{
-    gulp
-        .src(["./js/jQuery.js",
-              "./js/main.js"])
+    return gulp
+        .src(["./js/app/jQuery.js",
+              "./js/app/main.js"])
         .pipe(maps.init())
         .pipe(concat("app.js"))
         .pipe(maps.write("."))
         .pipe(gulp.dest("./js"));
 });
 
-gulp.task("uglifyScript",()=>{
-    gulp
+gulp.task("uglifyScript", ["concatScript"],()=>{
+    return gulp
         .src("./js/app.js")
         .pipe(uglify())
         .pipe(rename("app.min.js"))
@@ -29,7 +30,7 @@ gulp.task("uglifyScript",()=>{
 
 // gulp sass 
 gulp.task("sass",()=>{
-    gulp
+    return gulp
         .src("./scss/app.scss")
         .pipe(maps.init())
         .pipe(sass().on("error", sass.logError))
@@ -39,12 +40,25 @@ gulp.task("sass",()=>{
         .pipe(maps.write("."))
         .pipe(gulp.dest("./css"));
 });
-
-// gulp watch
-gulp.task("watch", ()=>{
-    gulp.watch("./js/*.js", ["concatScript", "uglifyScript"]);
-    gulp.watch("./**/*.scss", ["sass"]);
+// clean
+gulp.task("clean", ()=>{
+    del(["dist", "css/app.*", "js/app.*"]);
 });
 
-// default task
-gulp.task("default", ["concatScript", "uglifyScript", "sass", "watch"]);
+//  build
+gulp.task("build", ["uglifyScript", "sass"],()=>{
+    return gulp
+        .src(["./css/app.css", "./js/app.min.js", "index.html"], {'base': "."})
+        .pipe(gulp.dest("dist"));
+});
+
+// gulp watch
+gulp.task("watchFiles", ()=>{
+    gulp.watch("./**/*.scss", ["sass"]);
+    gulp.watch("./**/*.js", ["uglifyScript"]);
+});
+gulp.task("serve", ["watchFiles"]);
+//default
+gulp.task("default", ["clean"], ()=>{
+    gulp.start("build");
+});
